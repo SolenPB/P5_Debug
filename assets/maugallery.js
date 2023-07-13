21d6c9cdd8de895bec1531a305e441856d0b083b
SolenPB <126073154+SolenPB@users.noreply.github.com>
2023-07-13 11:02:07 +0200

    maj boutons catégories

--- assets/maugallery.js
+++ assets/maugallery.js
@@ -224,10 +224,10 @@
     },
     showItemTags(gallery, position, tags) {
       var tagItems =
-        '<button class="nav-item"><span class="nav-link active active-tag"  data-images-toggle="all">Tous</span></button>';
+        '<li class="nav-item"><span class="nav-link active active-tag"  data-images-toggle="all">Tous</span></li>';
       $.each(tags, function(index, value) {
-        tagItems += `<button class="nav-item active">
-                <span class="nav-link"  data-images-toggle="${value}">${value}</span></button>`;
+        tagItems += `<li class="nav-item active">
+                <span class="nav-link"  data-images-toggle="${value}">${value}</span></li>`;
       });
       var tagsRow = `<ul class="my-4 tags-bar nav nav-pills">${tagItems}</ul>`;
 
